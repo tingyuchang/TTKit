ebe948323d25df3515f6a8a498b1cb2ff1402231
tingyugenius <tingyugenius@gmail.com>
2013-09-09 11:39:32 +0800

    Initial Commit

---
+++ TTKit/TTAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  TTAppDelegate.h
+//  TTKit
+//
+//  Created by TinTint on 13/9/9.
+//  Copyright (c) 2013年 TinTint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ TTKit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TTKit
+//
+//  Created by TinTint on 13/9/9.
+//  Copyright (c) 2013年 TinTint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TTAppDelegate class]));
+    }
+}

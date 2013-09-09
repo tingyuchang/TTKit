43a6c7decdc745948b2d542c2161caae78a41585
tingyugenius <tingyugenius@gmail.com>
2013-09-09 12:32:29 +0800

    demo TTColor

--- TTKit/TTViewController.m
+++ TTKit/TTViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "TTViewController.h"
+#import <QuartzCore/QuartzCore.h>
+#import "TTColor.h"
 
 @interface TTViewController ()
 
@@ -26,7 +28,19 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+    
+    UIButton *changeColorBtn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [changeColorBtn setFrame:CGRectMake(0, 0, 100, 30)];
+    [changeColorBtn setCenter:CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height/2)];
+    [changeColorBtn setTitle:@"Change" forState:UIControlStateNormal];
+    [changeColorBtn addTarget:self action:@selector(changeColorByRand) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.view addSubview:changeColorBtn];
+                                          
+    
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -34,5 +48,30 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+#pragma mark --
+#pragma mark Private Method
+#pragma mark --
+
+
+- (void)changeColorByRand{
+    NSArray *colorAry = @[@"1abc9c", @"2ecc71", @"3498db", @"9b59b6", @"34495e", @"16a085", @"27ae60", @"2980b9", @"8e44ad", @"2c3e50", @"f1c40f", @"f39c12", @"e67e22", @"d35400", @"e74c3c", @"c0392b", @"ecf0f1", @"bdc3c7", @"95a5a6", @"7f8c8d"];
+    int randNum = rand()%[colorAry count];
+    
+    CATransition *fadeTransition = [CATransition animation];
+    [fadeTransition setDuration:0.3];
+    [fadeTransition setTimingFunction:[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut]];
+    fadeTransition.type = kCATransitionFade;
+    [fadeTransition setFillMode:kCAFillModeForwards];
+    [fadeTransition setRemovedOnCompletion:YES];
+    [self.view.layer addAnimation:fadeTransition forKey:@"fadeAnimation"];
+    [self.view setBackgroundColor:[TTColor colorFromHexRGB:[colorAry objectAtIndex:randNum]]];
+    
+    
+    
+
+}
+
 
 @end
